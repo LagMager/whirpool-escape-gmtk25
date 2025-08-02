882de4f093d582b592731413ef696695e0997491
cooling12 <agantquicksilverdogyreborn@gmail.com>
2025-08-02 02:12:43 +0100

    minor comments

--- gmtk-project/src/player/player.gd
+++ gmtk-project/src/player/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
-
+#the player
 @onready var player = $"."
 
 var ship_base = 24
 var steering_angle = 15 
 var speed = 260
-var rospeed = 2
+
+#needed for magnitude
 var mass = 0.5
 var angle = 0
 var radius = 0
-var point = Vector2(245,108)
+var centre_point = Vector2(245,108)
 var steer_direction
 var acceleration = Vector2.ZERO
 var friciton = -55
@@ -22,20 +23,27 @@ func _physics_process(delta):
 	calculate_steering(delta)
 	velocity += acceleration * delta
 	move_and_slide()
-	#circular_motion()
+	
 	#print(velocity.angle_to(point)) 
-	radius = player.position.distance_to(point)
+	radius = player.position.distance_to(centre_point)
 	#print(radius)
-	var directed_vector = point - player.position
+	#this creates a vector from the player to the centre point which is then normalised
+	var directed_vector = centre_point - player.position
 	var normalised_direction = directed_vector.normalized()
+	
+	#this allows for the contant motion 
 	var forward_vector = transform.x
 	velocity += forward_vector *  25 
 	#print(normalised_direction)
+	
+	#this is the centripetal force calcuations
 	var magnitude = (mass * directed_vector.length() **2)/radius 
+	#this protects from divide by 0 errors
 	if radius == 0:
 		magnitude = 0
 	
 	print(magnitude)
+	#this is is where the force is actually applied and where you can modify its strength 
 	var circle_round_force = normalised_direction * magnitude *3
 	velocity += circle_round_force * delta
 
@@ -65,14 +73,3 @@ func apply_friction(delta):
 	var friction_force = velocity * friciton * delta
 	var drag_force = velocity * velocity.length() * drag * delta
 	acceleration += drag_force + friction_force
-
-
-
-#func circular_motion():
-	#radius = player.position.distance_to(point)
-	##print(radius)
-	#var directed_vector = point - player.position
-	#var normalised_direction = directed_vector.normalized()
-	##print(normalised_direction)
-	#var magnitude = (mass * directed_vector.length() **2)/radius 
-	#print(magnitude)
